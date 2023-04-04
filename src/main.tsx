43262ec1ad53e1a3fe60f46df7e3ee6ae7102423
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-04 16:42:41 -0400

    adding Auth0

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import "./reset.css"
 import './index.css'
 import { RouterProvider } from "react-router-dom"
 import router from './router';
+import Auth0ProviderWithHistory from './auth/auth0Provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0ProviderWithHistory> {/* Wrap RouterProvider with Auth0ProviderWithHistory */}
+      <RouterProvider router={router} />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>,
 )
