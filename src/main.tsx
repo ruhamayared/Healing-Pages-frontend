6486a52d06ec239b807ea541986dc9b0c99745f8
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-06 19:00:25 -0400

    working on adding auth0

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "./reset.css"
+import './reset.css'
 import './index.css'
-import { RouterProvider } from "react-router-dom"
+import { RouterProvider } from 'react-router-dom'
 import router from './router';
+import Auth0ProviderWithHistory from './auth0-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0ProviderWithHistory>
+      <RouterProvider router={router} />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>,
-)
+);
