94bbd7a11510ff623e28493d84453f9c72ace2bf
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-06 19:38:12 -0400

    trying another auth library

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './reset.css'
+import "./reset.css"
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider } from "react-router-dom"
 import router from './router';
-import Auth0ProviderWithHistory from './auth0-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Auth0ProviderWithHistory>
-      <RouterProvider router={router} />
-    </Auth0ProviderWithHistory>
+    <RouterProvider router={router} />
   </React.StrictMode>,
-);
+)
